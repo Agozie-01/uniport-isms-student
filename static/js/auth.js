5f4a1735642a163eb104b6ae8c43de9362505eac
unknown <paulodion96@gmail.com>
2024-12-03 23:50:44 +0100

    Validate form data

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,5 +1,15 @@
  
- 
+// Define validation constraints
+const loginConstrants = {
+    username: {
+        presence: {allowEmpty: false, message: "is required"},
+    },
+    password: {
+      presence: {allowEmpty: false, message: "is required." },
+    }
+};
+
+
  // Handle form submission
  document.getElementById("login-form").onsubmit = async function(event) {
     event.preventDefault(); // Prevent default form submission (page reload)
@@ -10,6 +20,15 @@
         username: formData.get("username"),
         password: formData.get("password"),
     };
+
+    // Call validation function
+    const result = validateForm(loginData, loginConstrants);
+
+    // Display errors if validation fails
+    if (result) {
+        displayErrors(result);
+        return; // Prevent form submission if validation fails
+    }
 
     try {
         const response = await fetch('/api/token', {
